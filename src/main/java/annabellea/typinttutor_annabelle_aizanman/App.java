943f8f063fba0e37271d621c6cb23a7e9ed85791
annabelle <annabelleaiza@gmail.com>
2026-09-22 20:22:46 -0400

    stage setup continuation

--- src/main/java/annabellea/typinttutor_annabelle_aizanman/App.java
+++ src/main/java/annabellea/typinttutor_annabelle_aizanman/App.java
@@ -1,6 +1,8 @@
 package annabellea.typinttutor_annabelle_aizanman;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -17,16 +19,20 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        VBox root = new VBox();
+        VBox root = new VBox(20);
+        root.setPadding(new Insets(10));
+        root.setAlignment(Pos.TOP_CENTER);
         
         Label title = new Label("Typing Tutor");
-        //title.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
+        title.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
         
         Label instructionLabel = new Label("Text to type:");
         TextField input = new TextField();
+        HBox inputBox = new HBox(5, instructionLabel, input);
         
         Label responseLabel = new Label("Your text:");
         TextField response = new TextField();
+        HBox responseBox = new HBox(5, responseLabel, response);
         
         Label keyLabel = new Label("Key Pressed:");
         Label keyPressedLabel = new Label("None");
@@ -41,9 +47,9 @@ public class App extends Application {
         
         HBox control = new HBox(10, nextButton, resetButton);
         
-        root.getChildren().addAll(title, instructionLabel, input, responseLabel,
-                response, keyLabel, keyPressedLabel, correctKeysLabel, 
-                incorrectKeysLabel, counterLabel, control);
+        root.getChildren().addAll(title, inputBox, responseBox, keyLabel, 
+                keyPressedLabel, correctKeysLabel,incorrectKeysLabel,
+                counterLabel, control);
         
         var scene = new Scene(root, 640, 480);
         stage.setScene(scene);
